925ecec914d46d11766afa2b4c75d66cd6ece0eb
Ryan Baumann <ryan.baumann@gmail.com>
2018-02-19 14:49:20 -0500

    Use grunt connect for an HTTP server during tests, force non-async initial test load

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,26 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         qunit: {
-            files: ['src/test.html']
+          all: {
+            options: {
+              urls: ['http://localhost:8000/src/test.html']
+            }
+          }
+        },
+        connect: {
+          server: {
+            options: {
+              port: 8000,
+              base: '.'
+            }
+          }
         }
     });
 
     // Load plugin
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-qunit');
 
     // Task to run tests
-    grunt.registerTask('test', 'qunit');
+    grunt.registerTask('test', ['connect','qunit']);
 };
